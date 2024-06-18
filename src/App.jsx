ec2145b0533e5b50f1bdee4d80d70fc110ff430b
Kariuki Michael <kariukimichael017@gmail.com>
2024-06-18 12:39:29 +0300

    Adding header component to home and shop pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Homepage, Shop } from './pages';
+import { Header } from './components'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop />} />
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Directory from './directory/Directory';
 import MenuItems from './menu-items/MenuItems';
 import CollectionPreview from './collection-preview/CollectionPreview';
 import CollectionItem from './collection-item/CollectionItem';
+import Header from './header/Header'
 
 
 export {
     Directory,
     MenuItems,
     CollectionPreview,
-    CollectionItem
+    CollectionItem,
+    Header
 }
